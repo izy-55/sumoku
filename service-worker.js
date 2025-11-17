76b52acccc709b769cf3de2df4166722c2708476
izy-55 <izy4455@gmail.com>
2025-11-17 15:17:48 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "sumoku-cache-v1";
+const ASSETS = [
+  "./",
+  "./izy-sumoku_7x7-v3.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
